4b1069252ed3fdc2038e3b4288a398ecf37f762c
Sampreeth <sampreeth20@gmail.com>
2018-02-04 15:20:14 +0530

    Modified Annotation

--- source/Annotation.R
+++ source/Annotation.R
@@ -1,33 +1,10 @@
 suppressPackageStartupMessages(library(GenomicFeatures))
 suppressPackageStartupMessages(library(VariantAnnotation))
+suppressPackageStartupMessages(library(dplyr))
 fl <- ("hg18_refGene.gtf")
 txdb <- makeTxDbFromGFF(file=fl,format="gtf")
 fl <- system.file("extdata", "SRR1518358_default.vcf",package="VariantAnnotation")
 vcf <- readVcf('SRR1518358_default.vcf', "hg18")
-
-#head(seqlevels(vcf))
-#head(seqlevels(txdb))
-#intersect(seqlevels(vcf), seqlevels(txdb))
-
-#vcf <- renameSeqlevels(vcf, paste0("chr", seqlevels(vcf)))
-
-intersect(seqlevels(vcf), seqlevels(txdb))
-
-region <- IntergenicVariants(upstream=70000, downstream=70000)
-loc_int <- locateVariants(vcf, txdb, region)
-mcols(loc_int)[c("LOCATION", "PRECEDEID", "FOLLOWID")]
-
-p_ids <- unlist(loc_int$PRECEDEID, use.names=FALSE)
-exp_ranges <- rep(loc_int,  elementNROWS(loc_int$PRECEDEID))
-
-distance(exp_ranges, txdb, id=p_ids, type="gene")
-
-cdsbytx <- cdsBy(txdb)
-var_cds <-locateVariants(vcf, cdsbytx, CodingVariants())
-df <- data.frame()
-
-intbytx <- intronsByTranscript(txdb)
-locateVariants(vcf, intbytx, IntronVariants())
 
 fa <- open(FaFile('all.fa'))
 coding1 <- predictCoding(vcf,txdb,fa)
@@ -36,3 +13,34 @@ test <- final
 final <- test[,c("seqnames","start","ref","alt.value","type","gene_id")]
 ans <- final %>% group_by(seqnames,start,ref,alt.value,type) %>% summarise(gene_id = toString(gene_id))
 write.table(ans, file="foo.bed", quote=F, sep="\t", row.names=F, col.names=F)
+
+loc_all <- locateVariants(vcf, txdb, AllVariants())
+tets <- unique(loc_all)
+final <- data.frame(seqnames=seqnames(tets),start=start(tets),location=tets$LOCATION,gene_id=tets$GENEID,QUERYID=tets$QUERYID)
+region <- IntergenicVariants(upstream=70000, downstream=70000)
+loc_int <- locateVariants(vcf, txdb, region)
+test <- mcols(loc_int)[c("LOCATION", "PRECEDEID", "FOLLOWID","QUERYID")]
+xxx = CharacterList(character(0))
+for(i in 1:nrow(test))
+{
+  if(identical(test[i,"PRECEDEID"],xxx)){
+    test[i,"PRECEDEID"] = "NA"
+  }
+  else{
+    test[i,"PRECEDEID"]=paste(test[i,"PRECEDEID"],collapse=",")
+  }
+  if(identical(test[i,"FOLLOWID"],xxx)){
+    test[i,"FOLLOWID"] = "NA"
+  }
+  else{
+    test[i,"FOLLOWID"]=paste(test[i,"FOLLOWID"],collapse=",")
+  }
+}
+a <- merge(x=final,y=test,by = "QUERYID",all.x = TRUE)
+v <- rowRanges(vcf)
+var <- data.frame(seqnames=seqnames(v),start=start(v),REF=v$REF,ALT=v$ALT)
+var1 <- var[,c("seqnames","start","REF","ALT.value")]
+b <- merge(x=a,y=var1,by.x=c("seqnames", "start"),by.y=c("seqnames","start"),all.x = TRUE)
+new <- b[order(b$QUERYID),]
+c <- new[,c("seqnames","start","REF","ALT.value","location","gene_id","PRECEDEID","FOLLOWID")]
+write.table(c, file="test", quote=F, sep="\t", row.names=F, col.names=F)
